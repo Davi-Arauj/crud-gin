4445c2c03abdfdb2ee5a85c0a6ee2befebd085e6
Davi-Arauj <moreiradaavi@gmail.com>
2022-02-17 17:17:25 -0300

    Cliente Controler

--- controller/cliente-crontoller.go
+++ controller/cliente-crontoller.go
@@ -12,9 +12,9 @@ import (
 type ClienteController interface {
 	FindAll() []domain.Cliente
 	Save(ctx *gin.Context) error
+	Update(ctx *gin.Context) error
+	Delete(ctx *gin.Context) error
 	ShowAll(ctx *gin.Context)
-	Update(ctx *gin.Context)
-	FindById(ctx *gin.Context)
 }
 
 type controller struct {
@@ -22,6 +22,7 @@ type controller struct {
 }
 
 func New(service service.ClienteService) ClienteController {
+
 	return &controller{
 		service: service,
 	}
@@ -50,16 +51,35 @@ func (c *controller) ShowAll(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index.html", data)
 }
 
-func (c *controller) Update(ctx *gin.Context) {
+func (c *controller) Update(ctx *gin.Context) error {
+	var cliente domain.Cliente
+	err := ctx.ShouldBindJSON(&cliente)
+	if err != nil {
+		return err
+	}
 
+	id, er := strconv.ParseUint(ctx.Param("id"), 0, 0)
+	if er != nil {
+		return err
+	}
+
+	cliente.ID = id
+
+	c.service.Update(cliente)
+
+	return nil
 }
 
-func (c *controller) FindById(ctx *gin.Context) {
-	id, err := strconv.Atoi(ctx.Params.ByName("id"))
+func (c *controller) Delete(ctx *gin.Context) error {
+	var cliente domain.Cliente
+	id, err := strconv.ParseUint(ctx.Param("id"), 0, 0)
 
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
-	c.service.FindById(id)
 
+	cliente.ID = id
+
+	c.service.Delete(cliente)
+	return nil
 }
--- domain/cliente.go
+++ domain/cliente.go
@@ -2,11 +2,10 @@ package domain
 
 import (
 	"github.com/brisanet/cliente/enums"
-	"github.com/jinzhu/gorm"
 )
 
 type Cliente struct {
-	gorm.Model
+	ID          uint64 `gorm:"primary_key;auto_increment" json:"id"`
 	Name        string `json:"Nome" binding:"required,min=3,max=70"`
 	Email       string `json:"e-mail" binding:"required,email"`
 	Fone        int    `json:"fone"`
